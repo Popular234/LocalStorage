b8e358915d02b5f625c01ee0dcc2ead6d551a0f9
Popular234 <cainjose24@g.crookcountyschools.org>
2023-05-28 04:11:11 -0700

    finished project and attemped to use dom to display on html page, got an error I didnt understand how to fix

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 'use strict'
 
+console.log('hello world');
+
+
+const main = document.getElementById('main');
+
+const HandWrap = {
+    Brand: "RingSide",
+    Length: "180in",
+    color: "Black",
+    Style: "mexican-style",
+}
+
+// console.log(HandWrap);
+
+const JSONHandWrap = JSON.stringify(HandWrap);
+
+localStorage.setItem('HandWrap', JSONHandWrap)
+console.log(localStorage)
+
+let pissedOff = localStorage.getItem('HandWrap')
+
+console.log(pissedOff)
+
+const ma = ['Karate', 'Ju-Jitsu', 'Aikido', 'Kung-Fu', 'Muay Thai']
+
+console.log(ma)
+
+const man = JSON.stringify(ma);
+
+localStorage.setItem('ma', man);
+
+let mam = localStorage.getItem('ma');
+console.log(mam)
+// const JSONParse = JSON.parse(HandWrap);
+
+// main.appendChild(HandWrap)
